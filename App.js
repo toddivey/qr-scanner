2987169a765db67148bcdeb72e39c2f0218b66a2
Todd Ivey <toddivey@gmail.com>
2019-06-04 00:04:14 -0500

    multiple screens added

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
 
 import React, { Component } from "react";
-import {
-  Container,
-  Header,
-  Title,
-  Footer,
-  FooterTab,
-  Button,
-  Left,
-  Right,
-  Body,
-  Icon,
-  Text
-} from "native-base";
-
-import { Grid, Row } from "react-native-easy-grid";
-
-import { AppLoading, Constants } from "expo";
+import { AppLoading } from "expo";
+import Navigation from "./components/Navigation";
 
 export default class AnatomyExample extends Component {
   constructor(props) {
@@ -28,7 +13,7 @@ export default class AnatomyExample extends Component {
     await Expo.Font.loadAsync({
       Roboto: require("native-base/Fonts/Roboto.ttf"),
       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      // Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
     });
     this.setState({ loading: false });
   }
@@ -38,40 +23,6 @@ export default class AnatomyExample extends Component {
       return <AppLoading />;
     }
 
-    return (
-      <Container style={{ marginTop: Constants.statusBarHeight }}>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name="menu" />
-            </Button>
-          </Left>
-          <Body>
-            <Title>I AM A Header</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Grid
-          style={{
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <Row>
-            <Text>This is Content Section</Text>
-          </Row>
-          <Row>
-            <Text>Hello World</Text>
-          </Row>
-        </Grid>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
-    );
+    return <Navigation />;
   }
 }
