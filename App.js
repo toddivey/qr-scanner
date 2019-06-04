417845e4bb057134147f9bbd81ce6e478e07067f
Todd Ivey <toddivey@gmail.com>
2019-06-03 23:52:01 -0500

    app running on iphone and android

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Title,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text
+} from "native-base";
+
+import { Grid, Row } from "react-native-easy-grid";
+
+import { AppLoading, Constants } from "expo";
+
+export default class AnatomyExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      // Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+    this.setState({ loading: false });
+  }
+
   render() {
+    if (this.state.loading) {
+      return <AppLoading />;
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! Did this change? IT DID!</Text>
-      </View>
+      <Container style={{ marginTop: Constants.statusBarHeight }}>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>I AM A Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Grid
+          style={{
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <Row>
+            <Text>This is Content Section</Text>
+          </Row>
+          <Row>
+            <Text>Hello World</Text>
+          </Row>
+        </Grid>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
